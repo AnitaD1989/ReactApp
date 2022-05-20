d222c9961dc173a46d33898cfe5ae968cb6ed9bb
Anita Deszczynska <anitadeszczynska@gmail.com>
2022-05-20 14:31:35 +0200

    add map to List

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,6 +2,11 @@ import styles from './List.module.scss';
 import Column from '../Column/Column.js';
 
 const List = () => {
+  const columns = [
+  	{ id: 1, title: 'Books', icon: 'book' },
+    { id: 2, title: 'Movies', icon: 'film' },
+    { id: 3, title: 'Games', icon: 'gamepad' }
+  ];
   return (
     <div className={styles.list}>
       <header className={styles.header}>
@@ -9,9 +14,8 @@ const List = () => {
       </header>
       <p className={styles.description}>Intresting things I want to chek out!</p>
       <section className={styles.colums}>
-        <Column title="Books" span ='fa-fa-book'/>
-        <Column title="Movies" span = 'fa-fa-film'/>
-        <Column title="Games" span ='fa-fa-gamepad' />
+        {columns.map(column => 
+          <Column key={column.id} title={column.title} icon={column.icon} />)}
       </section>
     </div>
   );
