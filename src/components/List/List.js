b665d0760083c1e4dd36d1fbf3777f2dcf9af0fe
Anita Deszczynska <anitadeszczynska@gmail.com>
2022-05-23 19:56:33 +0200

    add columns

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,14 +1,30 @@
 import styles from './List.module.scss';
 import Column from '../Column/Column.js';
+import { useState } from 'react';
+import {shortid} from 'react';
 
 const List = () => {
-  const columns = [
+
+  const [columns, setColumns] = useState([
   	{ id: 1, title: 'Books', icon: 'book' },
     { id: 2, title: 'Movies', icon: 'film' },
     { id: 3, title: 'Games', icon: 'gamepad' }
-  ];
+  ]);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setColumns([...columns, { id: shortid(), title: value }]);
+    setValue('');
+  };
+
+  const [value, setValue] = useState('');
+
   return (
     <div className={styles.list}>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={value} onChange/>
+        <button>Add column</button>
+      </form>
       <header className={styles.header}>
         <h2 className={styles.title}>Things to do <span>soon</span></h2>
       </header>
